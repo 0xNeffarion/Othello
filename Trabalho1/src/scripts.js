99c03bd2a1a00e20323b829cc7fb8b370b055f22
Vasco Serrano <main@neffware.com>
2020-10-22 15:18:11 +0100

    Js

--- Trabalho1/src/scripts.js
+++ Trabalho1/src/scripts.js
@@ -1,1 +1,12 @@
-// JS
+const panels = new Array(6);
+
+panels[0] = "menu_1";
+panels[1] = "menu_2";
+panels[2] = "menu_3";
+panels[3] = "menu_4";
+panels[4] = "menu_5";
+panels[5] = "menu_6";
+
+function menuSwitch(id){
+    document.getElementById()
+}
